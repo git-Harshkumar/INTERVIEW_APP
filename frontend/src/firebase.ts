fc11242a1019e918c38f9c055af73c7c2eae1605
Your Name <your@email.com>
2026-06-03 00:11:07 +0530

    frontend designed

---
+++ frontend/src/firebase.ts
@@ -0,0 +1,30 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDXNPlEN_9Btebc1v_8iCaEv4D6RxW4V-g",
+  authDomain: "interview-ai-127f8.firebaseapp.com",
+  projectId: "interview-ai-127f8",
+  storageBucket: "interview-ai-127f8.firebasestorage.app",
+  messagingSenderId: "641314468288",
+  appId: "1:641314468288:web:5ab75406155eaf07c2d825",
+  measurementId: "G-LDGY3TPKRB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = typeof window !== 'undefined' ? getAnalytics(app) : null;
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+export const signInWithGoogle = async () => {
+  try {
+    const result = await signInWithPopup(auth, googleProvider);
+    return result.user;
+  } catch (error) {
+    console.error("Google Sign-In Error", error);
+    throw error;
+  }
+};
+export { auth, analytics } 
